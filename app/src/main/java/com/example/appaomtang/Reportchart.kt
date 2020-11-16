86291a67f54a0e5a28f1bd7d797356695b87ecc7
carrotmanfen <kudsun.khamjamnun@mail.kmutt.ac.th>
2020-11-16 19:51:19 +0700

    test234

--- app/src/main/java/com/example/appaomtang/Reportchart.kt
+++ app/src/main/java/com/example/appaomtang/Reportchart.kt
@@ -10,7 +10,7 @@ class Reportchart : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reportchart)d
+        setContentView(R.layout.activity_reportchart)
 
     }
 }
